2c3a44585fb99f8970fc504e1891051800b72674
khakisage <jongjin201@gmail.com>
2024-07-12 14:49:48 +0900

    create 2 files and update 1 file

--- src/router.tsx
+++ src/router.tsx
@@ -1,1 +1,15 @@
-export const Router = () => {};
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
+import { Provider } from "jotai";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Provider>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  );
+};
---
+++ src/state/atom/countAtom.tsx
@@ -0,0 +1,5 @@
+// count 상태 관리 로직을 atom으로 분리
+
+import { atom } from "jotai";
+
+export const countAtom = atom(0);
